a6601c2788262be9e7b48fbfd132b00a4f04a0f5
Samuel Zapolsky <samzapo@gmail.com>
2015-06-24 17:25:29 -0400

    parameter generation and random sampling w/ data output

--- src/Random.h
+++ src/Random.h
@@ -9,6 +9,7 @@
 #ifndef _Random_h
 #define _Random_h
 
+/*
 #include <random>
 
 class Generator {
@@ -30,6 +31,63 @@ public:
     double number;
     do{
       number = this->distribution(generator);
+    } while (number < this->min && number > this->max);
+    return number;
+  }
+};
+*/
+
+#include <gsl_rng.h>
+
+gsl_rng * r =  gsl_rng_alloc( gsl_rng_default );
+if( r == NULL ) std::cout << "failed to initialize rng\n";
+gsl_rng_set( r, seed );
+
+// generate command values from gaussian
+const double SIGMA = GAUSSIAN_STDDEV;
+const double MU = GAUSSIAN_MEAN;
+for( unsigned i = 0; i < 3; i++ ) {
+  walk_force[i] = ;
+  walk_torque[i] = 0.0;
+}
+
+// free the random number generator
+
+
+
+class Generator {
+  gsl_rng * generator;
+  std::normal_distribution<double> distribution;
+  double min;
+  double max;
+  double mu;
+  double sigma;
+public:
+  Generator(double mean, double stddev, double xmin, double xmax):
+  min(xmin),max(xmax),mu(mean),sigma(stddev)
+  {
+    r =  gsl_rng_alloc( gsl_rng_default );
+    if( r == NULL ) std::cout << "failed to initialize rng\n";
+    gsl_rng_set( r, seed );
+  }
+  
+  // Min and max are 3 std above and below the mean
+  Generator(double min, double max):
+  min(xmin),max(xmax),mu((min + max) / 2),sigma((max - min) / 6)
+  {
+    r =  gsl_rng_alloc( gsl_rng_default );
+    if( r == NULL ) std::cout << "failed to initialize rng\n";
+    gsl_rng_set( r, seed );
+  }
+  
+  ~Generator(){
+    gsl_rng_free( generator );
+  }
+  
+  double generate() {
+    double number;
+    do{
+      number = gsl_ran_gaussian( generator, sigma ) + mu;
     } while (number < this->min && number > this->max);
     return number;
   }
